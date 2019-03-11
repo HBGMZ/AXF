72d0cf382ac12cde7c3d64216705878de3c9f3b5
HBGMZ <hubo10010922@163.com>
2019-03-12 00:13:40 +0800

    注册登登录

--- HBAXF/app/models.py
+++ HBAXF/app/models.py
@@ -1,45 +1,32 @@
 from django.db import models
 
+# Create your models here.
 
-# 基础类
-class BaseModel(models.Model):
-    img = models.CharField(max_length=100)
-    name = models.CharField(max_length=100)
-    trackid = models.CharField(max_length=10)
-
+class Base(models.Model):
+    img=models.CharField(max_length=100)
+    name=models.CharField(max_length=100)
+    trackid=models.CharField(max_length=20)
     class Meta:
-        abstract = True
+        abstract=True
+    def __str__(self):
+        return self.name
 
-# 轮播图　模型类
-# insert into axf_wheel(img,name,trackid)
-class Wheel(BaseModel):
+
+class Wheel(Base):
     class Meta:
-        db_table = 'axf_wheel'
+        db_table='axf_wheel'
 
-
-# 导航　模型类
-class Nav(BaseModel):
+class Nav(Base):
     class Meta:
-        db_table = 'axf_nav'
-
-
-# 每日必购 模型类
-class Mustbuy(BaseModel):
+        db_table='axf_nav'
+class Mustbuy(Base):
     class Meta:
-        db_table = 'axf_mustbuy'
+        db_table='axf_mustbuy'
 
-
-# 部分商品 模型类
-class Shop(BaseModel):
+class Shop(Base):
     class Meta:
-        db_table = 'axf_shop'
+        db_table='axf_shop'
 
-
-# 商品列表　模型类
-# insert into
-# (,,,,,,,,,,,img2,childcid2,productid2,longname2,price2,marketprice2,img3,childcid3,productid3,longname3,price3,marketprice3)
-# values
-# (,"25.80","25.8","http://img01.bqstatic.com/upload/goods/201/611/1617/20161116173544_219028.jpg@200w_200h_90Q","103534","116950","蜂觅·越南直采红心火龙果350-450g/盒","15.3","15.8","http://img01.bqstatic.com/upload/goods/201/701/1916/20170119164119_550363.jpg@200w_200h_90Q","103533","118826","爱鲜蜂·海南千禧果400-450g/盒","9.9","13.8");
 class Mainshow(models.Model):
     trackid = models.CharField(max_length=10)
     name = models.CharField(max_length=100)
@@ -71,49 +58,67 @@ class Mainshow(models.Model):
     class Meta:
         db_table = 'axf_mainshow'
 
+class Foodtype(models.Model):
+    # 分类ID
+    typeid = models.CharField(max_length=10)
+    # 分类名称
+    typename = models.CharField(max_length=100)
+    # 子类(多个)
+    childtypenames = models.CharField(max_length=200)
+    # 排序
+    typesort = models.IntegerField()
 
-# 商品分类
-
-class Foodtypes(models.Model):
-    typeid = models.CharField(max_length=8) # 分类ID
-    typename = models.CharField(max_length=100) # 分类名称
-    childtypenames = models.CharField(max_length=256)   # 子类名称
-    typesort = models.IntegerField()    # 显示先后顺序
     class Meta:
         db_table = 'axf_foodtypes'
 
-# 商品信息
+
 class Goods(models.Model):
-    productid = models.CharField(max_length=10) # 商品ID
-    productimg = models.CharField(max_length=100)   # 商品图片
-    productname = models.CharField(max_length=100)  # 商品名称
-    productlongname = models.CharField(max_length=100)  # 商品弄名称
-    isxf = models.BooleanField(default=False)    # 精选
-    pmdesc = models.BooleanField(default=False)  # 买一送一
-    specifics = models.CharField(max_length=100)   # 规格
-    price = models.DecimalField(max_digits=7, decimal_places=2)    # 价格
-    marketprice = models.DecimalField(max_digits=7, decimal_places=2)  # 商场价格
-    categoryid= models.IntegerField()   # 分类ID
-    childcid = models.IntegerField()    # 子类ID
-    childcidname = models.CharField(max_length=100) # 分类名称
-    dealerid = models.CharField(max_length=10)  # 详情ID
-    storenums = models.IntegerField()   # 库存
-    productnum = models.IntegerField()  # 销量
+    # 商品ID
+    productid = models.CharField(max_length=10)
+    # 商品图片
+    productimg = models.CharField(max_length=100)
+    # 商品名称
+    productname = models.CharField(max_length=100)
+    # 商品长名字
+    productlongname = models.CharField(max_length=256)
+    # 是否精选
+    isxf = models.IntegerField()
+    # 是否买一送一
+    pmdesc = models.IntegerField()
+    # 商品规格
+    specifics = models.CharField(max_length=100)
+    # 商品价格
+    price = models.DecimalField(max_digits=6,decimal_places=2)
+    # 商品超市价格
+    marketprice = models.DecimalField(max_digits=6, decimal_places=2)
+    # 分类ID
+    categoryid = models.IntegerField()
+    # 子类ID
+    childcid = models.IntegerField()
+    # 子类名称
+    childcidname = models.CharField(max_length=100)
+    # 详情页ID
+    dealerid = models.CharField(max_length=10)
+    # 库存
+    storenums = models.IntegerField()
+    # 销售
+    productnum = models.IntegerField()
 
     class Meta:
         db_table = 'axf_goods'
 
 class User(models.Model):
-#     账号
-    account  = models.CharField(max_length=80,unique=True)
-# 密码
+    # 邮箱
+    email = models.CharField(max_length=40, unique=True)
+    # 密码
     password = models.CharField(max_length=256)
-    #name
+    # 昵称
     name = models.CharField(max_length=100)
-    phone = models.CharField(max_length=20,unique=True)
-    addr = models.CharField(max_length=256)
-    img = models.CharField(max_length=100)
+    # 头像
+    img = models.CharField(max_length=40, default='axf.png')
+    # 等级
     rank = models.IntegerField(default=1)
-    token = models.CharField(max_length=256)
+    token=models.CharField(max_length=255,default='')
+
     class Meta:
-        db_table = 'axf_user'
+         db_table = 'axf_user'
--- HBAXF/app/views.py
+++ HBAXF/app/views.py
@@ -1,88 +1,155 @@
 import hashlib
-import os
-import uuid
+import random
+import time
 
-from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render, redirect
 
 # Create your views here.
-from axf.models import Wheel, Nav, Mustbuy, Shop, MainShow, Foodtypes, Goods, User, Cart
-from py1809AXF import settings
+from axf.models import Wheel, Nav, Mustbuy, Shop, Mainshow, Foodtype, Goods, User
 
 
 def home(request):
-    wheels = Wheel.objects.all()
-    navs = Nav.objects.all()
-    mustbuys = Mustbuy.objects.all()
-    #商品
-    shopList = Shop.objects.all()
-    shophead = shopList[0]
-    shoptab = shopList[1:3]
-    shopclass = shopList[3:7]
-    shopcommend = shopList[7:11]
-    #商品主题
-    mainshows = MainShow.objects.all()
-    data ={
+    wheels=Wheel.objects.all()
+    navs=Nav.objects.all()
+    mustbuys=Mustbuy.objects.all()
+    shops=Shop.objects.all()
+    shophead=shops[0]
+    shoptabs=shops[1:3]
+    shopclass_list=shops[3:7]
+    shopcommends=shops[7:11]
+    mainshows=Mainshow.objects.all()
+    data={
         'wheels':wheels,
         'navs':navs,
         'mustbuys':mustbuys,
         'shophead':shophead,
-        "shoptab":shoptab,
-        "shopclass":shopclass,
-        "shopcommend":shopcommend,
-        "mainshows":mainshows,
-
-
+        'shoptabs':shoptabs,
+        'shopclass_list':shopclass_list,
+        'shopcommends':shopcommends,
+        'mainshows':mainshows
     }
-    return render(request,'home/home.html',context=data)
 
-# 购物车
-def cart(request):
-    token = request.session.get('token')
-    # carts = []
-    if token:
-        # 显示该用户下 购物车信息
-        user = User.objects.get(token=token)
-        carts = Cart.objects.filter(user=user).exclude(number=0)
-        return render(request,'cart/cart.html',context={'carts':carts})
+
+    return render(request,'home/home.html' ,context=data)
+
+
+def market(request,childid='0',sortid='0'):
+    foodtypes=Foodtype.objects.all()
+
+    # goods_list=Goods.objects.all()[0:5]
+    # goods_list = Goods.objects.filter(categoryid=categoryid)
+    index=int(request.COOKIES.get('index','0'))
+    categoryid=foodtypes[index].typeid
+
+    if childid == '0':
+        goods_list = Goods.objects.filter(categoryid=categoryid)
     else:
-        # 跳转到登录页面
-        return redirect('axf:login')
+        goods_list = Goods.objects.filter(categoryid=categoryid).filter(childcid=childid)
 
-
-def market(request, categoryid, childid, sortid):    # 闪购超市
-    # 分类信息
-    foodtypes = Foodtypes.objects.all()
-
-    # 分类 点击 下标
-    typeIndex = int(request.COOKIES.get('typeIndex', 0))
-    # 根据分类下标
-    categoryid = foodtypes[typeIndex].typeid
-
-    # 子类信息
-    childtypenames = foodtypes.get(typeid=categoryid).childtypenames
-    # 将每个子类拆分出来
-    childTypleList = []
-    for item in childtypenames.split('#'):
-        arr = item.split(':')
-        dir = {
-            'childname': arr[0],
-            'childid': arr[1]
-        }
-        childTypleList.append(dir)
-
-    # 商品信息
-    # goodsList = Goods.objects.all()[0:5]
-    if childid == '0':  # 全部分类
-        goodsList = Goods.objects.filter(categoryid=categoryid)
-    else:   # 分类
-        goodsList = Goods.objects.filter(categoryid=categoryid, childcid=childid)
-
-
-    # 排序
     if sortid == '1':
-        goodsList = goodsList.order_by('-productnum')
+        goods_list = goods_list.order_by('-productnum')
     elif sortid == '2':
-        goodsList = goodsList.order_by('price')
+        goods_list = goods_list.order_by('price')
     elif sortid == '3':
-        goodsList = goodsList.order_by(('-price'))
+        goods_list = goods_list.order_by('-price')
+
+    # if childid == '0':
+    #     goods_list = Goods.objects.filter(categoryid=categoryid)
+    # else:
+    #     goods_list = Goods.objects.filter(categoryid=categoryid).filter(childcid=childid)
+
+    # goods_list=Goods.objects.filter(categoryid=categoryid)
+
+
+    childtypenames=foodtypes[index].childtypenames
+    childtype_list=[]
+    for item in childtypenames.split('#'):
+        item_arr=item.split(':')
+        temp_dir={
+            'name':item_arr[0],
+            'id':item_arr[1]
+        }
+        childtype_list.append(temp_dir)
+    response_dir = {
+        'foodtypes': foodtypes,
+        'goods_list': goods_list,
+        'childtype_list':childtype_list,
+        'childid': childid
+    }
+    return render(request,'market/market.html',context=response_dir)
+
+
+def mine(request):
+
+    token=request.session.get('token')
+    user=None
+    if token:
+        user=User.objects.get(token=token)
+
+
+
+    return render(request,'mine/mine.html',context={'user':user})
+
+
+def cart(request):
+    return render(request,'cart/cart.html')
+
+
+def generate_token():
+    token = str(time.time()) + str(random.random())
+    md5 = hashlib.md5()
+    md5.update(token.encode('utf-8'))
+
+    return md5.hexdigest()
+
+
+def generate_password(param):
+    md5 = hashlib.md5()
+    md5.update(param.encode('utf-8'))
+    return md5.hexdigest()
+
+
+def register(request):
+    if request.method == "GET":
+        return render(request, 'mine/register.html')
+    elif request.method == "POST":
+        email=request.POST.get('email')
+        password=request.POST.get('password')
+        name=request.POST.get('name')
+        token=generate_token()
+        try:
+            user=User()
+            user.email=email
+            user.password=generate_password(password)
+            user.name=name
+            user.token=token
+            user.save()
+            response=redirect('axf:mine')
+            request.session['token']=user.token
+
+            return response
+        except:
+            return render(request,'mine/register.html')
+
+
+def login(request):
+    if request.method=='GET':
+        return render(request,'mine/login.html')
+    elif request.method=='POST':
+        name=request.POST.get('name')
+        password=request.POST.get('password')
+        users=User.objects.filter(name=name).filter(password=password)
+        if users.exists():
+            user=users.first()
+            user.token=generate_token()
+
+            user.save()
+            request.session['token']=user.token
+
+            return  redirect('axf:mine')
+        else:
+            return  render(request,'mine/login.html')
+
+def logout(request):
+    request.session.flush()
+    return redirect('axf:mine')
